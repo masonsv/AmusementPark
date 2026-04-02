95d5ad46bae318622c8a806cbdb58779b71aef96
masonsv <masonsvagle@gmail.com>
2026-04-02 13:07:17 -0500

    small error

--- AmusementPark/src/AmusementPark.java
+++ AmusementPark/src/AmusementPark.java
@@ -2,7 +2,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.*;
 
-public class Company {
+public class AmusementPark {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
