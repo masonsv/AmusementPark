fa96f3d7ab0b5918d935702c17a3ab3b9d234551
Jacob Hansen <jakhansen12@MacBookPro.student.uwlax.edu>
2026-04-02 13:56:46 -0500

    forgot to save
    Signed-off-by: Jacob Hansen <jakhansen12@MacBookPro.student.uwlax.edu>

--- AmusementPark/bin/jdbc.sql
+++ AmusementPark/bin/jdbc.sql
@@ -1,56 +1,56 @@
-CREATE DATABASE ssfoley_Company_JDBC;
+/*CREATE DATABASE ssfoley_Company_JDBC;
 
-USE ssfoley_Company_JDBC;
+USE ssfoley_Company_JDBC;*/
 
-/* Create Employee table */
-CREATE TABLE Employee (
-  SSN CHAR(11) PRIMARY KEY NOT NULL,-- NVARCHAR(11) NVARCHAR(9) INT
-  Salary NUMERIC(8,2) NOT NULL,
-  FirstName NVARCHAR(25) NOT NULL,
-  MiddleName NVARCHAR(25),
-  LastName NVARCHAR(25) NOT NULL
+CREATE TABLE Customer (
+	CustomerID int NOT NULL auto_increment,
+    FirstName varchar(255),
+    LastName varchar(255),
+    ThrillLevel int,
+    Height int NOT NULL,
+    Budget double,
+    Age int,
+    TicketType varchar(255) NOT NULL,
+    PRIMARY KEY (CustomerID),
+    CONSTRAINT fk_ticket_type FOREIGN KEY (TicketType) REFERENCES Ticket (TicketType)
 );
 
-/* Create Project table */
-CREATE TABLE Project(
-  ID INTEGER AUTO_INCREMENT,
-  Description NVARCHAR(255),
-  PRIMARY KEY (ID) -- other style of specifying the primary key, useful for composite keys
+CREATE TABLE Ride (
+	RideID int NOT NULL auto_increment,
+    RideName varchar(255) NOT NULL,
+    HeightRequirement int,
+    RideType varchar(255),
+    RideTime int,
+    ThrillLevel int,
+    Capacity int,
+    AvgWaitTime double,
+    Rating double,
+    PRIMARY KEY (RideID, RideName)
 );
 
-/* Create Works_On */
-CREATE TABLE Works_On(
-  ID INTEGER NOT NULL,
-  SSN CHAR(11) NOT NULL,
-  Hours INTEGER NOT NULL,
-  CONSTRAINT fk_project_id FOREIGN KEY (ID) REFERENCES Project(ID),
-  CONSTRAINT fk_employee_ssn FOREIGN KEY (SSN) REFERENCES Employee(SSN)
+CREATE TABLE RIDE_ON (
+	RideID int,
+    RideName varchar(255),
+    CustomerID int,
+    CONSTRAINT fk_ride_id FOREIGN KEY (RideID) REFERENCES Ride (RideID),
+    CONSTRAINT fk_ride_name FOREIGN KEY (RideName) REFERENCES Ride (RideName),
+    CONSTRAINT fk_customer_id FOREIGN KEY (CustomerID) References Customer (CustomerID)
 );
 
-/* Add data */
-INSERT INTO Employee(SSN, Salary, FirstName, LastName) VALUES ('123-45-6789', 123445.00, 'Jasper', 'Fforde');
-INSERT INTO Employee(SSN, Salary, FirstName, MiddleName, LastName) VALUES ('123-45-6799', 13445.00, 'Robin', 'Wall', 'Kimmerer');
-INSERT INTO Employee(SSN, Salary, FirstName, LastName) VALUES ('123-45-9876', 600000.00, 'Ada', 'Lovelace');
-INSERT INTO Employee(SSN, Salary, FirstName, MiddleName, LastName) VALUES ('123-54-6799', 903445.00, 'Grace', 'Murray', 'Hopper');
-INSERT INTO Employee(SSN, Salary, FirstName, MiddleName, LastName) VALUES ('123-54-9967', 133445.00, 'Octavia', 'E.', 'Butler');
+CREATE TABLE CarnivalGame (
+	GameID int NOT NULL auto_increment,
+    PrizeID int,
+    Price double,
+    GameName varchar(255) NOT NULL,
+    WinOdds double,
+    PRIMARY KEY (GameID, PrizeID)
+);
 
-
-INSERT INTO Project(Description) VALUES ('A test project.');
-INSERT INTO Project(Description) VALUES ('Another test project.');
-INSERT INTO Project(Description) VALUES ('Project Awesomesauce');
-INSERT INTO Project(Description) VALUES ('Dumpster fire');
-INSERT INTO Project(Description) VALUES ('Dodos for fun');
-
-
-INSERT INTO Works_On(ID, SSN, Hours) VALUES (4, '123-45-6799', 3);
-INSERT INTO Works_On(ID, SSN, Hours) VALUES (3, '123-45-9876', 33);
-INSERT INTO Works_On(ID, SSN, Hours) VALUES (3, '123-54-6799', 13);
-INSERT INTO Works_On(ID, SSN, Hours) VALUES (1, '123-45-6799', -3);
-INSERT INTO Works_On(ID, SSN, Hours) VALUES (3, '123-45-6789', 5);
-INSERT INTO Works_On(ID, SSN, Hours) VALUES (1, '123-54-6799', 3);
-INSERT INTO Works_On(ID, SSN, Hours) VALUES (5, '123-45-6789', 37);
-INSERT INTO Works_On(ID, SSN, Hours) VALUES (4, '123-54-9967', 15);
-INSERT INTO Works_On(ID, SSN, Hours) VALUES (5, '123-54-9967', 3);
-
-SELECT * FROM Project NATURAL JOIN Works_On NATURAL JOIN Employee ORDER BY SSN;
-
+CREATE TABLE FoodStall (
+	StallID int NOT NULL auto_increment,
+    Price double,
+    StallName varchar(255),
+    FoodType varchar(255),
+    AmountSold int,
+    PRIMARY KEY (StallID)
+);
--- AmusementPark/src/Customer.java
+++ AmusementPark/src/Customer.java
@@ -1,63 +1,94 @@
 
-public class Employee {
+public class Customer {
 
-	private String ssn;
-	private double salary;
-	private String firstName;
-	private String middleName;
-	private String lastName;
+	private int CustomerID;
+	private int ThrillLevel;
+	private int Height;    //inches
+	private int Age;
+	private double Budget; //$xxx.xx
+	private String TicketType;
+	private String FirstName;
+	private String LastName;
 	
-	public Employee(String ssn, double salary, String firstName, String middleName, String lastName) {
+	public Customer(int CustomerID, int ThrillLevel, int Height, int Age, double Budget, String TicketType, String FirstName, String LastName) {
 		super();
-		this.ssn = ssn;
-		this.salary = salary;
-		this.firstName = firstName;
-		this.middleName = middleName;
-		this.lastName = lastName;
+		this.CustomerID = CustomerID;
+		this.ThrillLevel = ThrillLevel;
+		this.Height = Height;
+		this.Age = Age;
+		this.Budget = Budget;
+		this.TicketType = TicketType;
+		this.FirstName = FirstName;
+		this.LastName = LastName;
 	}
 
-	public double getSalary() {
-		return salary;
+	public int getCustomerID() {
+		return CustomerID;
 	}
 
-	public void setSalary(double salary) {
-		this.salary = salary;
+	public void getCustomerID(int CustomerID) {
+		this.CustomerID = CustomerID;
+	}
+
+	public int getThrillLevel() {
+		return ThrillLevel;
+	}
+
+	public void getThrillLevel(int ThrillLevel) {
+		this.ThrillLevel = ThrillLevel;
+	}
+
+	public int getHeight() {
+		return Height;
+	}
+
+	public void getHeight(int Height) {
+		this.Height = Height;
+	}
+	public int getAge() {
+		return Age;
+	}
+
+	public void getAge(int Age) {
+		this.Age = Age;
+	}
+	public double getBudget() {
+		return Budget;
+	}
+
+	public void getBudget(double Budget) {
+		this.Budget = Budget;
+	}
+
+	public String getTicketType() {
+		return TicketType;
+	}
+
+	public void setTicketType(String TicketType) {
+		this.TicketType = TicketType;
 	}
 
 	public String getFirstName() {
-		return firstName;
+		return FirstName;
 	}
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getMiddleName() {
-		return middleName;
-	}
-
-	public void setMiddleName(String middleName) {
-		this.middleName = middleName;
+	public void setFirstName(String FirstName) {
+		this.FirstName = FirstName;
 	}
 
 	public String getLastName() {
-		return lastName;
+		return LastName;
 	}
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getSsn() {
-		return ssn;
+	public void setLastName(String LastName) {
+		this.LastName = LastName;
 	}
 
 	@Override
 	public String toString() {
-		return "Employee [ssn=" + ssn + ", salary=" + salary + ", firstName=" + firstName + ", middleName=" + middleName
-				+ ", lastName=" + lastName + "]";
+		return "Customer [CustomerID=" + CustomerID + ", ThrillLevel=" + ThrillLevel + ", Height=" + Height + ", Age=" + Age
+				+ ", Budget=" + Budget + ", TicketType=" + TicketType + ", FirstName=" + FirstName + ", LastName=" + LastName +"]";
 	}
-	
 	
 	
 }
